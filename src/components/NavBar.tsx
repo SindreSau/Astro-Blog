38997b41490acdfe252c5c1b5073ab5939e02b1a
Sindre Sauarlia <101213882+SindreSau@users.noreply.github.com>
2023-10-02 09:26:41 +0200

    commit before restart

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react';
-import { BiMenu, BiX } from 'react-icons/bi';
+import { motion } from 'framer-motion';
 
 const NavBar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState('Home');
 
-  const className = isMenuOpen ? 0 : 100;
+  useEffect(() => {
+    const path = window.location.pathname;
+    const pathName = path === '/' ?? 'posts' ? 'home' : path.slice(1);
+    setCurrentPage(pathName);
+  }, []);
 
   const navItems = [
     {
@@ -23,11 +27,34 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className='flex'>
-        <ul className='gap-6 font-bold flex'>
+      <nav>
+        {/* Desktop */}
+        <ul className='hidden gap-6 sm:flex'>
           {navItems.map((item, index) => (
-            <li key={index}>
-              <a href={item.path}>{item.name}</a>
+            <li
+              key={index}
+              className={
+                item.name.toLowerCase() === currentPage ? 'border-b border-slate-600 dark:border-slate-200' : ''
+              }>
+              <a className='text-lg md:text-xl transition-colors duration-100 dark:text-white' href={item.path}>
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        {/* Mobile */}
+        <button className='text-white flex flex-col gap-1.5 sm:hidden'>
+          <motion.div className='w-8 h-0.5 rounded-full bg-white'></motion.div>
+          <motion.div className='w-8 h-0.5 rounded-full bg-white'></motion.div>
+          <motion.div className='w-8 h-0.5 rounded-full bg-white'></motion.div>
+        </button>
+        <ul className='absolute md:hidden left-full '>
+          {navItems.map((item, index) => (
+            <li key={index} className={item.name.toLowerCase() === currentPage ? 'border-b' : ''}>
+              <a className='text-lg md:text-xl transition-colors duration-100 ' href={item.path}>
+                {item.name}
+              </a>
             </li>
           ))}
         </ul>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,30 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {
       colors: {
         'dark-background': '#1a1a1a',
-        'light-background': '#f5f5f5',
+        'dark-background-secondary': '#2a2a2a',
+        'dark-darker': '#121212',
+        'light-background': '#fff',
+        'light-background-secondary': '#e5e5e5',
+        'light-darker': '#ccccda',
+      },
+
+      primary: {
+        light: '#ff0',
+        DEFAULT: '#ff0',
+        dark: '#abc',
       },
     },
   },
