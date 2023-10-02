5e847117c3191b9fae413f3d53e2106bec098c1f
Sindre Sauarlia <101213882+SindreSau@users.noreply.github.com>
2023-10-02 15:30:52 +0200

    navmenu responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,17 +6,29 @@ module.exports = {
       backgroundColor: {
         primary: 'var(--color-background)',
         secondary: 'var(--color-background-secondary)',
+        accent: 'var(--color-background-accent)',
+        'accent-hover': 'var(--color-text-accent-hover)',
       },
       textColor: {
         primary: 'var(--color-text)',
         muted: 'var(--color-text-muted)',
         accent: 'var(--color-text-accent)',
+        'accent-hover': 'var(--color-text-accent-hover)',
+      },
+      borderColor: {
+        primary: 'var(--color-text)',
+        muted: 'var(--color-border-muted)',
+        accent: 'var(--color-text-accent)',
       },
       fontFamily: {
         base: ['Lato', 'sans-serif'],
-        heading: ['Cabin Sketch', 'cursive'],
+        heading: ['Playfair Display', 'serif'],
       },
     },
+  },
+  // New feature to fix hover on touch devices
+  future: {
+    hoverOnlyWhenSupported: true,
   },
   plugins: [],
 };
